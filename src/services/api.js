bc8d50d1a6914792387f9a32d38c8e53d89631b1
DarkSoulsMan <joaodarksouls3dasilva@gmail.com>
2025-10-15 18:41:42 -0400

    Testando

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Porta do teu backend
+  baseURL: "http://localhost:3000", // Porta do teu backend (krl)
 });
 
 export default api;
